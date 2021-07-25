a5fcef9fc0ec84a6218ba395af1d942d63470b9e
Sofia Fateyev <pateyev.sofia@gmail.com>
2021-07-25 23:48:01 +0100

    Add react router

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import { store } from './store';
 
 export const App = () => {
-  return <Provider store={store}></Provider>;
+  return (
+    <Provider store={store}>
+      <Router>
+        <Switch>
+          <Route path="/"></Route>
+        </Switch>
+      </Router>
+    </Provider>
+  );
 };
