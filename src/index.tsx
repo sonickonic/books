8e140e3dd57f7f7d1b93e0a6d7cb4b845338c8ee
Sofia Fateyev <pateyev.sofia@gmail.com>
2021-07-26 00:39:49 +0100

    Add ErrorBoundary class component
    On render errors - display alternative UI and log error.

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import { App } from './app';
+import { ErrorBoundary } from './components/error-boundary/error-boundary-component';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
   </React.StrictMode>,
   document.getElementById('root')
 );
