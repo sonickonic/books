0dba7e3d54239b2f459268e97f187e23e3b68039
Sofia Fateyev <pateyev.sofia@gmail.com>
2021-07-25 23:49:33 +0100

    Add hook to retrieve query params from URL

---
+++ src/hooks/useQuery.ts
@@ -0,0 +1,19 @@
+import { useLocation } from 'react-router-dom';
+
+export const DEFAULT_PAGE = 1;
+export const DEFAULT_ITEMS_PER_PAGE = 10;
+
+export const useQuery = () => {
+  const query = new URLSearchParams(useLocation().search);
+
+  const page = Number(query.get('page')) || DEFAULT_PAGE;
+  const itemsPerPage =
+    Number(query.get('itemsPerPage')) || DEFAULT_ITEMS_PER_PAGE;
+
+  const filtersValues = new Array(query.get('q')).filter(Boolean) as string[];
+  const filters = filtersValues.length
+    ? [{ type: 'all', values: filtersValues }]
+    : [];
+
+  return { page, itemsPerPage, filters };
+};
